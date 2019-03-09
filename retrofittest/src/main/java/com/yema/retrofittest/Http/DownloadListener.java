ed4cabe50566908d9df310ba24e53f514bed5df3
张德亮 <zhangdl@yemast.com>
2019-03-09 17:08:15 +0800

    完善下载模块

---
+++ retrofittest/src/main/java/com/yema/retrofittest/Http/DownloadListener.java
@@ -0,0 +1,20 @@
+package com.yema.retrofittest.Http;
+
+interface DownloadListener {
+
+    /**
+     * 下载成功
+     */
+    void onDownloadSuccess(String path);
+
+    /**
+     * @param progress
+     * 下载进度
+     */
+    void onDownloading(int progress);
+
+    /**
+     * 下载失败
+     */
+    void onDownloadFailed();
+}
--- retrofittest/src/main/java/com/yema/retrofittest/Http/UrlApi.kt
+++ retrofittest/src/main/java/com/yema/retrofittest/Http/UrlApi.kt
@@ -1,13 +1,12 @@
 package com.yema.retrofittest.Http
 
-import android.widget.TextView
 import com.yema.retrofittest.dataclass.ServerInfoResponse
 import com.yema.retrofittest.dataclass.UploadFileModel
 import okhttp3.MultipartBody
-import okhttp3.RequestBody
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.*
+
 
 interface UrlApi {
     @GET("/get")
@@ -25,4 +24,9 @@ interface UrlApi {
     @Streaming
     @GET("/note/youdaonote_android_6.5.1_youdaoweb.apk")
     fun downFile(): Call<ResponseBody>
+
+
+    @Streaming //大文件时要加不然会OOM
+    @GET
+    fun downloadFile(@Url fileUrl: String): Call<ResponseBody>
 }
