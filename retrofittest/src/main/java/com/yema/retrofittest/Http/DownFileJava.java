de6721d96951dfff7615557260189110bc9fb5ca
张德亮 <zhangdl@yemast.com>
2019-03-09 17:07:02 +0800

    Recycleview异常

--- retrofittest/src/main/java/com/yema/retrofittest/Http/DownFileJava.java
+++ retrofittest/src/main/java/com/yema/retrofittest/Http/DownFileJava.java
@@ -1,15 +1,18 @@
 package com.yema.retrofittest.Http;
 
 import android.app.Activity;
-import android.content.Context;
+import android.os.Environment;
 import android.util.Log;
-import android.widget.TextView;
 
-import org.w3c.dom.Text;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 import okhttp3.ResponseBody;
+import retrofit2.Response;
 
-import java.io.*;
 
 public class DownFileJava {
 
@@ -63,4 +66,62 @@ public class DownFileJava {
         }
     }
 
+    public  static  void writeResponseBodyToDisk2(final String url, final String saveDir,  Response<ResponseBody> response, DownloadListener downloadListener) throws IOException {
+        InputStream is = null;
+        byte[] buf = new byte[2048];
+        int len = 0;
+        FileOutputStream fos = null;
+        // 储存下载文件的目录
+        String saveFolder = isExistDir(saveDir);
+        String tempPath = saveFolder + File.separator + "temp_" + getNameFromUrl(url);
+        String savePath = saveFolder + File.separator + getNameFromUrl(url);
+        try {
+            FileUtils.deleteFile(tempPath);
+            is = response.body().byteStream();
+            long total = response.body().contentLength();
+            File file = new File(tempPath);
+            fos = new FileOutputStream(file);
+            long sum = 0;
+            while ((len = is.read(buf)) != -1) {
+                fos.write(buf, 0, len);
+                sum += len;
+                int progress = (int) (sum * 1.0f / total * 100);
+                // 下载中
+                downloadListener.onDownloading(progress);
+            }
+            fos.flush();
+            // 下载完成
+            FileUtils.fileRename(tempPath, savePath);
+            downloadListener.onDownloadSuccess(savePath);
+        } catch (Exception e) {
+            downloadListener.onDownloadFailed();
+        } finally {
+            try {
+                if (is != null)
+                    is.close();
+            } catch (IOException e) {
+            }
+            try {
+                if (fos != null)
+                    fos.close();
+            } catch (IOException e) {
+            }
+        }
+    }
+
+
+    private static String isExistDir(String saveDir) throws IOException {
+        // 下载位置
+        File downloadFile = new File(Environment.getExternalStorageDirectory(), saveDir);
+        if (!downloadFile.mkdirs()) {
+            downloadFile.createNewFile();
+        }
+        String savePath = downloadFile.getAbsolutePath();
+        return savePath;
+    }
+
+
+    public static String getNameFromUrl(String url) {
+        return url.substring(url.lastIndexOf("/") + 1);
+    }
 }
